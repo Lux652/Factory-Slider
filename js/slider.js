c5e62bc349cec465d6644b163b23811b74efe0ec
Luka <lukakovacicster@gmail.com>
2018-04-16 17:09:05 +0200

    Final upload

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,4 @@
+//funkcija za slider(jQuery plugin Slick https://kenwheeler.github.io/slick/)
 $('.slider').slick({
     infinite: true,
     speed: 700,
@@ -7,6 +8,29 @@ $('.slider').slick({
     prevArrow:$('.previous'),
     nextArrow:$('.next')
 });
+
+//iako se klase isto zovu, funkcija se pozove samo na zadnju klasu
+
+/*$('.slider-second-class').slick({
+    infinite: true,
+    speed: 700,
+    slidesToShow: 3,
+    centerMode: true,
+    variableWidth: true,
+    prevArrow:$('.previous'),
+    nextArrow:$('.next')
+});
+$('.slider-first-class').slick({
+    infinite: true,
+    speed: 700,
+    slidesToShow: 3,
+    centerMode: true,
+    variableWidth: true,
+    prevArrow:$('.previous'),
+    nextArrow:$('.next')
+});
+
+U ovom slucaju bi gumb radio samo na prvi red slidera */
 
 
 //funkcija za mijenjanje property-a i ikone gumba za prethodni slide
